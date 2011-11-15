3c4084608df79f2149be0087c659979b28d24e83
Etti <etti.nekov@gmail.com>
2011-11-15 18:11:16 +0400

    Libs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Libs::Application.config.secret_token = '30d0987741d28d853e2056e4f4754ab7b762e48086253f396c5b1626e2748a74ef7fe6360bb25adbbb491c0f766d1397c0eed86000f38d9dea2ac08a10f815bd'
